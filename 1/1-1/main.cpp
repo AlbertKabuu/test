6b61bf0208593d82f999ca7b5084c70bca7ec820
fool <lazyrrrrererere@gmail.com>
2024-03-23 02:23:24 +0400

    добавил вывод значений

--- 1/1-1/main.cpp
+++ 1/1-1/main.cpp
@@ -7,5 +7,7 @@ int main() {
 	Road road;
 	road.lenght = 11;
 	road.Width = 1;
+	std::cout << "lenght" << road.lenght << std::endl;
+	std::cout << "Width:"<< road.Width << std::endl;
 	return(0);
 }
