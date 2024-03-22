fba630406536b9213462de9bf1780735d3c7ae95
fool <lazyrrrrererere@gmail.com>
2024-03-23 03:21:56 +0400

    присваивание значений и вывод онных

--- 1/1-4/main.cpp
+++ 1/1-4/main.cpp
@@ -11,5 +11,13 @@ public:
 int main() {
 	TBook book1;
 
+	book1.name = "Alices Adventures in Wonderland";
+	book1.author = "Lewis Carroll";
+	book1.count = "1";
+
+	cout << "Name: " << book1.name << endl;
+	cout << "Author: " << book1.author << endl;
+	cout << "Count: " << book1.count << endl;
+
 	return 0;
 }
