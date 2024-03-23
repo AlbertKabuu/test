bbfbb58baa11b2e6313b9b95404c2e012d837874
fool <lazyrrrrererere@gmail.com>
2024-03-23 04:25:58 +0400

    добавил метод для присвоения значенйи из текстового файла

--- 2/2-3/main.cpp
+++ 2/2-3/main.cpp
@@ -7,5 +7,24 @@ class Road {
 		Road() {
 			length = 0;
 			width = 0;
-		}	
+		}
+		void setParameters(){
+			ifstream file;
+			
+			file.open("config.txt");
+			if (file.is_open()) {
+				string line;
+				
+				getline(file, line);
+				size_t slashPos = line.find("\\");
+				string firstNumber = line.substr(0, slashPos);
+				length = stoi(firstNumber);
+
+				getline(file, line);
+				string secondNumber = line.substr(0, slashPos);
+				width = stoi(secondNumber);
+
+				file.close();
+			}
+		}
 };
