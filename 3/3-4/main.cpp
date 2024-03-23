a619a709625b3b243dc8ade00f329e120db6a337
fool <lazyrrrrererere@gmail.com>
2024-03-23 06:30:57 +0400

    добавлена функция для заполнения объектов books

--- 3/3-4/main.cpp
+++ 3/3-4/main.cpp
@@ -4,3 +4,29 @@ public:
 	string Author;
 	int Count;
 };
+
+int main() {
+	const int s = 3;
+	TBook books[s];
+	ifstream fin;
+	fin.open("config.txt");
+	if (fin.is_open()) {
+		for (int i = 0; i < s; i++) {
+			string temp;
+			getline(fin, books[i].Name);
+			getline(fin, books[i].Author);
+			getline(fin, temp);
+			books[i].Count = stoi(temp);
+		}
+		fin.close();
+	}
+	else return 1;
+
+	
+
+	
+
+	
+
+	return 0;
+}
