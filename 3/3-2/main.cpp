efa4eda45d1218c7736406286ca4e71d2189ba40
fool <lazyrrrrererere@gmail.com>
2024-03-23 05:56:22 +0400

    добавлены конструкторы

--- 3/3-2/main.cpp
+++ 3/3-2/main.cpp
@@ -2,8 +2,17 @@ class Car {
 	private:
 		int x, p, v;
 		
-	
-		
+	public:
+		Car() {
+			x = 0;
+			p = 0;
+			v = 0;
+		}
+		Car(int x, int p, int v) {
+			this->x = x;
+			this->p = p;
+			this->v = v;
+		}
 		
 		
 		
